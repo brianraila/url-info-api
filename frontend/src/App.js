1a6d60fe4583e840ee47364c4a6d03d0c09f1035
DESKTOP-FJAKAS7\Siddhi <siddhigate16@gmail.com>
2022-08-07 14:00:01 +0530

    refactor: folder structure

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,32 +1,9 @@
+import Landing from "./components/Landing";
+
 function App() {
   return (
     <div className="App">
-      <header className="header">
-        <div className="flex-center">
-          <img className="logo" src="./broken-link.png"  alt="logo"></img>
-          <div className="logo-name">link preview</div>
-        </div>
-      </header>
-
-      <main id="main">
-        <div className="main-wrapper">
-          <div className="main-left">
-          <img className="logo-title" src="./broken-link.png" alt="logo"></img>
-            <h1>Link Preview</h1>
-            <p className="main-desc">
-              View the link preview using our App. Use the API to generate link
-              preview in your app.
-            </p>
-            <div className="ctas">
-            <button className="know-more-btn">Try it out</button>
-            <button className="know-more-btn know-more-btn-secondary">Try the API</button>
-            </div>
-          </div>
-          <div className="main-right">
-            <img src="./headerlink.svg"  alt="logo" className="mockup" />
-          </div>
-        </div>
-      </main>
+      <Landing/>
     </div>
   );
 }
